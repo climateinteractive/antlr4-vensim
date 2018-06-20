f6910e342ba89c6744dae0cd48fd62aae5ed9f40
Todd Fincannon <todd@toddfincannon.com>
2018-06-19 17:44:29 -0700

    improve the lex token printout with regexps

--- tools/print-tokens.js
+++ tools/print-tokens.js
@@ -1,15 +1,25 @@
 const getStdin = require('get-stdin')
-const B = require('bufx')
+const { pr, lines } = require('bufx')
 
 // Print an ANTLR 4 lex listing from stdin.
+let readToken = token => {
+  let result = ''
+  let re = /\[@(\d+),(\d+):(\d+)='([^']+)',(<[^>]+>),(\d+):(\d+)\]/
+  let m = token.match(re)
+  if (m) {
+    let seq = m[1]
+    let iStart = m[2]
+    let iEnd = m[3]
+    let str = m[4]
+    let tok = m[5]
+    let line = m[6]
+    let col = m[7]
+    result = `${tok}\t${str}\tline ${line}\tcol ${col}`
+  }
+  return result
+}
 getStdin().then(data => {
-  for (let line of B.lines(data)) {
-    B.pr(line
-      .replace(/\[@[^\']+\'/, '')
-      .replace(/\',/, '\t')
-      .replace(/>,/, '>\tline ')
-      .replace(/:/, '\tcol ')
-      .replace(/\]$/, '')
-    )
+  for (let line of lines(data)) {
+    pr(readToken(line))
   }
 })
